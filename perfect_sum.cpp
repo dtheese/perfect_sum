7f300c6396d5a3633e4a47cd6183040e42edd4c0
David Theese <dtheese@rocky-1.home.theese.com>
2023-05-08 17:53:28 -0700

    Cosmetic cleanup

--- perfect_sum.cpp
+++ perfect_sum.cpp
@@ -66,11 +66,11 @@ namespace
    // Command line arguments and flags
    bool display_group_count{false};
    bool duplicates_allowed{false};
-   ostream_up_t output_stream{&cout, ostream_conditional_deleter{false}};
    bool permutations_allowed{false};
    bool zeros_allowed{false};
-   my_uint_t K{0};
+   ostream_up_t output_stream{&cout, ostream_conditional_deleter{false}};
    my_uint_t N{0};
+   my_uint_t K{0};
 };
 
 int main(int argc, char *argv[])
